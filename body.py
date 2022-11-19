8e34604140422775796a91563cbd1115623cc2d3
yeryerix <96430806+yeryerix@users.noreply.github.com>
2022-11-19 13:59:50 +0700

    Add files via upload

--- body.py
+++ body.py
@@ -1,21 +1,40 @@
 print ('Задача на размер звукового файла')
-need = input("Что нужно найти? A- размер файла / D- частота дискретизации / Т- длинна записи / I- битовая глубина? - ")
-if need == 'A' or need == 'А':
+need = input("Что нужно найти? \n  A - размер файла (Мб) \n  D - частота дискретизации (Гц)\n  Т - длинна записи (сек)\n  I - битовая глубина? (бит)\n- ")
+if need == 'A' or need == 'А' or need == 'a' or need == 'а' or need == 'Ф' or need == 'ф' or need == 'F' or need == 'f':
     print('Дано:')
-    needAD = int (input('D- частота дискретизации Гц = '))
-    needAT = int (input('Т- длинна записи сек = '))
-    needAI = int (input('I- битовая глубина бит = '))
+    needAD = int (input('D - частота дискретизации (Гц) = '))
+    needAT = int (input('Т - длинна записи (сек) = '))
+    needAI = int (input('I - битовая глубина (бит) = '))
     needACh = int (input ('Сколько каналов? - '))
     needA = (needACh*needAD*needAT*needAI)/8/1024/1024
-    print(f"А= {needA} (Мбайт)")
- 
-elif need != 'A' or need !='А':
-    print (" [ERROR] - Tы что-то не так ввёл, перезапусти программу и введи заново")
+    print(f"А = {needA} (Мбайт) \nРабота закончена...")
+
+elif need == 'D' or need == 'd' or need == 'в' or need == 'В':
+    print('Дано:')
+    needDA = int (input('A - размер файла (Мб) = '))
+    needDT = int (input('Т - длинна записи (сек) = '))
+    needDI = int (input('I - битовая глубина (бит) = '))
+    needDCh = int (input ('Сколько каналов? - '))
+    needD = (needDA/(needDCh*needDT*needDI))
+    print(f'D = {needD} (Гц) \nРабота закончена...')
+
+elif need == 'T' or need == 't' or need == 'Т' or need == 'т' or need == 'Е' or need == 'е' or need == 'N' or need == 'n':
+    print('Дано:')
+    needTA = int (input('A - размер файла (Мб) = '))
+    needTD = int (input('D - частота дискретизации (Гц) = '))
+    needTI = int (input('I - битовая глубина (бит) = '))
+    needTCh = int (input ('Сколько каналов? - '))
+    needT = (needTA/(needTCh*needTD*needTI))
+    print(f'T = {needT} (сек) \nРабота закончена...')
+
+elif need == 'I' or need == 'i' or need == 'Ш' or need == 'ш':
+    print('Дано:')
+    needIA = int (input('A - размер файла (Мб) = '))
+    needID = int (input('D - частота дискретизации (Гц) = '))
+    needIT = int (input('I - битовая глубина (бит) = '))
+    needICh = int (input ('Сколько каналов? - '))
+    needI = (needIA/(needICh*needID*needIT))
+    print(f'I = {needI} (бит) \nРабота закончена...')
 
 else:
-    if need == 'D':
-        print('Дано:')
-        needAD = int (input('D- частота дискретизации Гц = '))
-        needAT = int (input('Т- длинна записи сек = '))
-        needAI = int (input('I- битовая глубина бит = '))
-        needACh = int (input ('Сколько каналов? - '))
+    print (" [ERROR] - Tы что-то не так ввёл, перезапусти программу и введи заново")
