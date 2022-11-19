612858103f24979d59cc34725326dbef665ca624
yeryerix <96430806+yeryerix@users.noreply.github.com>
2022-11-19 14:26:46 +0700

    Add files via upload

--- body.py
+++ body.py
@@ -11,29 +11,29 @@ if need == 'A' or need == 'А' or need == 'a' or need == 'а' or need == 'Ф' or
 
 elif need == 'D' or need == 'd' or need == 'в' or need == 'В':
     print('Дано:')
-    needDA = int (input('A - размер файла (Мб) = '))
+    needDA = float (input('A - размер файла (Мб) = '))
     needDT = int (input('Т - длинна записи (сек) = '))
     needDI = int (input('I - битовая глубина (бит) = '))
     needDCh = int (input ('Сколько каналов? - '))
-    needD = (needDA/(needDCh*needDT*needDI))
+    needD = (needDA/(needDCh*needDT*needDI)*8*1024*1024)
     print(f'D = {needD} (Гц) \nРабота закончена...')
 
 elif need == 'T' or need == 't' or need == 'Т' or need == 'т' or need == 'Е' or need == 'е' or need == 'N' or need == 'n':
     print('Дано:')
-    needTA = int (input('A - размер файла (Мб) = '))
+    needTA = float (input('A - размер файла (Мб) = '))
     needTD = int (input('D - частота дискретизации (Гц) = '))
     needTI = int (input('I - битовая глубина (бит) = '))
     needTCh = int (input ('Сколько каналов? - '))
-    needT = (needTA/(needTCh*needTD*needTI))
+    needT = (needTA/(needTCh*needTD*needTI)*8*1024*1024)
     print(f'T = {needT} (сек) \nРабота закончена...')
 
 elif need == 'I' or need == 'i' or need == 'Ш' or need == 'ш':
     print('Дано:')
-    needIA = int (input('A - размер файла (Мб) = '))
+    needIA = (input('A - размер файла (Мб) = '))
     needID = int (input('D - частота дискретизации (Гц) = '))
-    needIT = int (input('I - битовая глубина (бит) = '))
+    needIT = int (input('Т - длинна записи (сек)'))
     needICh = int (input ('Сколько каналов? - '))
-    needI = (needIA/(needICh*needID*needIT))
+    needI = (needIA/(needICh*needID*needIT)*8*1024*1024)
     print(f'I = {needI} (бит) \nРабота закончена...')
 
 else:
